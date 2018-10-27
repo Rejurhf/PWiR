3143680698c826d9084307a0c6dc03740f0a39f7
Rejurhf <mziomek108@gmail.com>
2018-10-27 23:30:45 +0200

    Random Generator Done

--- Ada/lab03/lab4lista.adb
+++ Ada/lab03/lab4lista.adb
@@ -1,5 +1,5 @@
-with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
-use Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Float_Random;
+with Ada.Text_IO, Ada.Integer_Text_IO, Ada.Numerics.Discrete_Random;
+use Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure Lab4Lista is
     type Element is
@@ -70,7 +70,17 @@ procedure Lab4Lista is
         end loop;
     end Insert_Sort;
 
-    
+    procedure Random_Insert(List : in out Elem_Ptr; N : in Integer) is
+        subtype Numery is Integer range 0..99;
+        package Los_Liczby is new Ada.Numerics.Discrete_Random(Numery);
+        use Los_Liczby;
+        Gen: Generator;
+    begin
+        Reset(Gen);
+        for I in 1..N loop
+            Insert_Sort(List, Random(Gen));
+        end loop;
+    end Random_Insert;
 
 
     Lista : Elem_Ptr := Null;
@@ -81,7 +91,7 @@ procedure Lab4Lista is
       Print(Lista);
       Insert(Lista, 20);
       Print(Lista);
-      for I in reverse 1..12 loop
+      for I in reverse 1..5 loop
       Insert(Lista, I);
       end loop;
       Print(Lista);
@@ -90,5 +100,8 @@ procedure Lab4Lista is
       Insert_Sort(Lista, 0);
       Insert_Sort(Lista, 25);
       Insert_Sort(Lista, 18);
+      Print(Lista);
+
+      Random_Insert(Lista, 5);
       Print(Lista);
 end Lab4Lista;
