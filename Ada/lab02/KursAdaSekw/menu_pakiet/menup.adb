696ed66d8f9d12ea65200588db0f1853624ba39d
Rejurhf <mziomek108@gmail.com>
2018-11-05 12:27:04 +0100

    Log In Progress

--- Ada/lab02/KursAdaSekw/menu_pakiet/menup.adb
+++ Ada/lab02/KursAdaSekw/menu_pakiet/menup.adb
@@ -1,35 +1,40 @@
 -- menup.adb
 
-with Ada.Text_IO, Opcje;
-use Ada.Text_IO, Opcje;
+with Ada.Text_IO, Opcje, Ada.Calendar;
+use Ada.Text_IO, Opcje, Ada.Calendar;
 
 procedure MenuP is
-  Zn: Character := ' '; 
-  
+  Zn: Character := ' ';
+  Pl: File_Type;
+  Name: String := "dziennik.txt";
+
   procedure Pisz_Menu is
   begin
-	New_Line;  
-	Put_Line("Menu:");  
+	New_Line;
+	Put_Line("Menu:");
     Put_Line(" s - opcja s");
 	Put_Line(" c - opcja c");
 	Put_Line(" p - opcja p");
 	Put_Line("ESC -Wyjscie");
 	Put_Line("Wybierz (s,c,p, ESC-koniec):");
   end Pisz_Menu;
-  
+
 begin
-  loop
-    Pisz_Menu;
-	Get_Immediate(Zn);
-	exit when Zn = ASCII.ESC;
-	case Zn is
-	  when 's' => Opcja_S;
-	  when 'c' => Opcja_C;
-	  when 'p' => Opcja_P;
-      when others => Put_Line("Blad!!");
-	end case;
-  end loop;
-  Put_Line("Koniec");
+    Create(Pl, Out_File, Name);
+
+    Put_Line(Pl, "Data");
+    loop
+        Pisz_Menu;
+        Get_Immediate(Zn);
+        Put_Line(Pl, Zn'Img);
+        exit when Zn = ASCII.ESC;
+        case Zn is
+            when 's' => Opcja_S;
+            when 'c' => Opcja_C;
+            when 'p' => Opcja_P;
+            when others => Put_Line("Blad!!");
+        end case;
+    end loop;
+    Put_Line("Koniec");
+    Close(Pl);
 end MenuP;
-  	 
-  
