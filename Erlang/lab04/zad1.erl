d5a308d2c3cac14b7e7b93f8cd52731532a2fb39
Rejurhf <mziomek108@gmail.com>
2018-11-05 14:38:52 +0100

    Objetosci Done

--- Erlang/lab04/zad1.erl
+++ Erlang/lab04/zad1.erl
@@ -1,7 +1,15 @@
 -module(zad1).
--export([pole/1]).
+-import(math,[pi/0]).
+-export([pole/1, objetosc/1]).
 
 pole({kwadrat,X,Y}) ->  X*Y;
 pole({trojkat,A,H}) ->  A*H/2;
 pole({trapez,A,B,H}) ->  (A+B)*H/2;
-pole({kolo,X}) -> 3.14*X*X.
+pole({kolo,X}) -> X*X*pi();
+pole({kula,R}) -> 4*R*R*pi();
+pole({szescian,A}) ->  6*A*A;
+pole({stozek,R,L}) ->  (R+L)*R*pi().
+
+objetosc({kula,R}) -> 4/3*R*R*R*pi();
+objetosc({szescian,A}) ->  A*A*A;
+objetosc({stozek,R,H}) ->  1/3*R*H*pi().
