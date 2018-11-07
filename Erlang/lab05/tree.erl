3443bc00b6e477e30a94ea34202f0d8102d7e885
Rejurhf <mziomek108@gmail.com>
2018-11-07 15:13:31 +0100

    Tree Insert Done

---
+++ Erlang/lab05/tree.erl
@@ -0,0 +1,16 @@
+-module(tree).
+-export([empty/0, insert/2, lookup/2]).
+
+empty() -> {node, 'nil'}.
+insert(Val, {node, 'nil'}) ->
+   {node, {Val, {node, 'nil'}, {node, 'nil'}}};
+insert(NewVal, {node, {Val, Smaller, Larger}}) when NewVal < Val ->
+   {node, {Val, insert(NewVal, Smaller), Larger}};
+insert(NewVal, {node, {Val, Smaller, Larger}}) when NewVal >= Val ->
+   {node, {Val, Smaller, insert(NewVal, Larger)}}.
+
+lookup(_, {node, 'nil'}) -> undefined;
+lookup(Val, {node, {Val, _, _}}) -> {ok, Val};
+lookup(Val, {node, {NodeVal, Smaller, _}}) when Val < NodeVal ->
+   lookup(Val, Smaller);
+lookup(Val, {node, {_, _, Larger}}) -> lookup(Val, Larger).
